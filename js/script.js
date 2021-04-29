d67ffa177185d11a888c5ed99e81bd047cd6d8eb
Patrycja-dz <patrycja.dziemianko@wp.pl>
2021-04-29 23:41:45 +0200

    created few functions

--- js/script.js
+++ js/script.js
@@ -1,44 +1,53 @@
-let form = document.querySelector('.js-form');
-let form__field = document.querySelector(".js-form__field");
-let form__select = document.querySelector(".js-form__select");
-let form__submitButton = document.querySelector(".js-form__buttonSubmit");
-let form__resetButton = document.querySelector(".js-form__buttonReset");
-let form__paragraph = document.querySelector(".js-form__paragraph");
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let finalSentence = "Otrzymasz";
-    let currency = form__select.value;
-    let plnValue = +form__field.value;
-    let dollarRate = 3.7920;
-    let frankRate = 4.1298;
-    let funtRate = 5.2351;
-    let euroRate = 4.5416;
-    let result;
-    switch (currency) {
-        case "USD":
-            result = (plnValue / dollarRate).toFixed(2);
-            form__paragraph.innerText = `${finalSentence} ${result} dolarów`;
-            break;
-        case "CHF":
-            result = (plnValue / frankRate).toFixed(2);
-            form__paragraph.innerText = `${finalSentence} ${result} franków`;
-            break;
-        case "GBP":
-            result = (plnValue / funtRate).toFixed(2);
-            form__paragraph.innerText = `${finalSentence} ${result} funtów`;
-            break;
-        case "EUR":
-            result = (plnValue / euroRate).toFixed(2);
-            form__paragraph.innerText = `${finalSentence} ${result} euro`;
-            break;
-        default:
-            form__paragraph.innerText = "Coś poszło nie tak, naprawiamy to!";
+{
 
+    const calculateResult = (plnValue, currency) => {
+        const dollarRate = 3.7920;
+        const frankRate = 4.1298;
+        const funtRate = 5.2351;
+        const euroRate = 4.5416;
+        switch (currency) {
+            case "USD":
+                return (plnValue / dollarRate).toFixed(2);
+
+
+            case "CHF":
+                return (plnValue / frankRate).toFixed(2);
+
+
+            case "GBP":
+                return (plnValue / funtRate).toFixed(2);
+
+
+            case "EUR":
+                return (plnValue / euroRate).toFixed(2);
+
+
+        };
+    };
+
+    const updateResultText = (plnValue, result, currency) => {
+        const form__paragraph = document.querySelector(".js-form__paragraph");
+        form__paragraph.innerHTML = `${plnValue} PLN = <strong> ${result} ${currency} </strong>`;
     }
 
-    form.addEventListener("reset", () => {
-        form__paragraph.innerText = `${finalSentence}`;
-        console.log("formularz został zresetowany");
 
-    });
-});
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const form__field = document.querySelector(".js-form__field");
+        const form__select = document.querySelector(".js-form__select");
+        const currency = form__select.value;
+        const plnValue = +form__field.value;
+        const result = calculateResult(plnValue, currency);
+        updateResultText(plnValue, result, currency)
+    }
+
+    const init = () => {
+        const form = document.querySelector('.js-form');
+        form.addEventListener("submit", onFormSubmit);
+        
+    }
+
+    init();
+}
